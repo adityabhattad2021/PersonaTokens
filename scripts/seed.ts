35cb85971f5480b493adcce5b0bd6ed7771051bf
Aditya Bhattad <adityabhattad18@gmail.com>
2023-08-10 18:12:16 +0530

    feat: connect to database and create category schema

---
+++ scripts/seed.ts
@@ -0,0 +1,30 @@
+const { PrismaClient } = require('@prisma/client');
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: 'Famous People' },
+        { name: 'Game Characters' },
+        { name: 'Animals' },
+        { name: 'Philosophy' },
+        { name: 'Friendship' },
+        { name: 'Coaching' },
+      ],
+    });
+  } catch (error) {
+    console.log('Error seeding default categories:', error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main()
+  .then(() => {
+    console.log('Seeding categories successful!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
