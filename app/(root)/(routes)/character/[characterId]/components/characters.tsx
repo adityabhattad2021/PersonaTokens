d44bf91086108292bd2d4ec44f62bd12daac2558
Aditya Bhattad <adityabhattad18@gmail.com>
2023-08-11 12:36:31 +0530

    feat: add character card

---
+++ app/(root)/(routes)/character/[characterId]/components/characters.tsx
@@ -0,0 +1,80 @@
+import { Card, CardFooter, CardHeader } from "@/components/ui/card"
+import type { Character } from "@prisma/client"
+import { MessageSquare } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+
+interface CharacterProps {
+    data: (Character & {
+        _count: {
+            messages: number
+        }
+    })[]
+}
+
+export default function Characters({ data }: CharacterProps) {
+
+    if(data.length===0){
+        return (
+            <div className="pt-10 flex flex-col items-center jsutify-center space-y-3">
+                <div className="relative w-60 h-60">
+                    <Image
+                        fill
+                        className="grayscale"
+                        alt="Empty"
+                        src="/empty.png"
+                    />
+                </div>
+                <p>
+                    No character found.
+                </p>
+            </div>
+        )
+    }
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 pb-10">
+            {
+                data.map((item)=>{
+                    return (
+                        <Card
+                            key={item.id}
+                            className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+                        >
+                            <Link href={`/chat/${item.id}`}>
+                                <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+                                    <div className="relative w-32 h-32">
+                                        <Image
+                                            fill
+                                            src={item.src}
+                                            alt="Character Image"
+                                            className="rounded-xl object-cover"
+                                        />
+                                    </div>
+                                    <p className="font-bold">
+                                        {item.name}
+                                    </p>
+                                    <p className="text-xs">
+                                        {item.description}
+                                    </p>
+                                </CardHeader>
+                                <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+                                    <p className="lowercase">
+                                        @0xabc
+                                    </p>
+                                    <div className="flex items-center">
+                                        <MessageSquare
+                                            className="w-3 h-3 mr-1"
+                                        />
+                                        {item._count.messages}
+                                    </div>
+                                </CardFooter>
+                            </Link>
+                        </Card>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,17 +1,48 @@
 import Categories from "@/components/categories";
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
+import Characters from "./character/[characterId]/components/characters";
 
-export default async function RootPage() {
+
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  }
+}
+
+export default async function RootPage({ searchParams }: RootPageProps) {
+
+  const data = await prismadb.character.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        }
+      }
+    }
+  })
 
   const categoires = await prismadb.category.findMany();
 
   return (
     <div className="h-full p-4 space-y-2">
-        <SearchInput/>
-        <Categories
-          data={categoires}
-        />
+      <SearchInput />
+      <Categories
+        data={categoires}
+      />
+      <Characters
+        data={data}
+      />
     </div>
   )
 }
